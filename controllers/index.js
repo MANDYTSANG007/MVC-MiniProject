91899952d1ea6242de030ea8d2e8d0ba4de6611c
MChambersIV <chambersm025@gmail.com>
2022-02-08 14:55:01 -0800

    added homepage route, updated package manifest with dependencies

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+const homePage = require('./homepageRoute');
+
+router.use('/', homePage);
 router.use('/api', apiRoutes);
+
 
 module.exports = router;
